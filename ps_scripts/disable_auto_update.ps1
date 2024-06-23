b03bdec6e3c66bb5b5f0c64c4b1e23e425b779e9
Ninjarku <33929885+Ninjarku@users.noreply.github.com>
2024-06-23 16:08:59 +0800

    Create disable_auto_update.ps1

---
+++ ps_scripts/disable_auto_update.ps1
@@ -0,0 +1,21 @@
+# From https://gist.github.com/mikebranstein/7e9169000a6555c195043e1755fbee7e
+# set the Windows Update service to "disabled"
+sc.exe config wuauserv start=disabled
+
+# display the status of the service
+sc.exe query wuauserv
+
+# stop the service, in case it is running
+sc.exe stop wuauserv
+
+# display the status again, because we're paranoid
+sc.exe query wuauserv
+
+# double check it's REALLY disabled - Start value should be 0x4
+REG.exe QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\wuauserv /v Start
+
+
+# Setting it to 1 means disabled (To disable autoupdate)
+$AUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+$AUSettings.NotificationLevel = 1
+$AUSettings.Save
