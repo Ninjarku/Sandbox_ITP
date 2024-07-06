9e57a63dab33764700073b7eeeb874b469f7488d
Ninjarku <33929885+Ninjarku@users.noreply.github.com>
2024-07-06 16:11:27 +0800

    Update Rename-KVMRegistryKeys.ps1

--- ps_scripts/Rename-KVMRegistryKeys.ps1
+++ ps_scripts/Rename-KVMRegistryKeys.ps1
@@ -5,13 +5,13 @@ function Rename-BlacklistedRegistryKeysAndExecutables {
     )
 
     # Check if registry key exists
-    if (Test-Path "HKLM:\$RegKey") {
+    try {
         # Rename registry key
         Rename-Item -Path "HKLM:\$RegKey" -NewName "Renamed_$RegKey" -Force
 
         # Check if executable path exists in registry key
         $key = Get-ItemProperty -Path "HKLM:\$RegKey" -ErrorAction SilentlyContinue
-        if ($key -ne $null -and $key.ImagePath -ne $null) {
+        if ($null -ne $key -and $null -ne $key.ImagePath) {
             $executablePath = $key.ImagePath
             # Check if executable exists and rename it
             if (Test-Path $executablePath -PathType Leaf) {
@@ -20,7 +20,7 @@ function Rename-BlacklistedRegistryKeysAndExecutables {
             }
         }
     }
-    else {
+    catch {
         Write-Host "Registry key '$RegKey' does not exist."
     }
 }
