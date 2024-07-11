286103fa6631606ac8beb82b1c4767b4d17d2e30
Ninjarku <33929885+Ninjarku@users.noreply.github.com>
2024-07-11 11:55:37 +0800

    reordering scripts

---
+++ ps_scripts/Modify-ACPIStrings .ps1
@@ -0,0 +1,17 @@
+# Function to modify ACPI table strings
+function Modify-ACPIStrings {
+    $acpiTables = Get-ACPI # Custom function to retrieve ACPI tables, you'll need to implement this part
+
+    foreach ($table in $acpiTables) {
+        if ($table.Content -match "BOCHS|BXPC") {
+            $table.Content = $table.Content -replace "BOCHS", "MODIFIED"
+            $table.Content = $table.Content -replace "BXPC", "MODIFIED"
+        }
+    }
+
+    # Apply modified tables
+    Set-ACPI -Tables $acpiTables # Custom function to set ACPI tables, you'll need to implement this part
+}
+
+# Execute the modification
+Modify-ACPIStrings
