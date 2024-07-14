b231c3270d4e930eee600614d704fe6a33bb6ac3
szehui <114173250+tszehui@users.noreply.github.com>
2024-07-14 14:35:39 +0800

    Mousemovement

---
+++ ps_scripts/Mouse_movement.ps1
@@ -0,0 +1,25 @@
+# Constants for mouse events
+$MOUSE_EVENT = 0x0001
+
+# Function to move the mouse with random offsets and sleep duration
+function Move_Mouse {
+    param (
+        [int]$minOffset = -10,
+        [int]$maxOffset = 10,
+        [int]$minSleep = 1,
+        [int]$maxSleep = 5
+    )
+
+    $xOffset = Get-Random -Minimum $minOffset -Maximum $maxOffset
+    $yOffset = Get-Random -Minimum $minOffset -Maximum $maxOffset
+
+    [UserInput]::mouse_event($MOUSE_EVENT, $xOffset, $yOffset, 0, 0)
+
+    $sleepSeconds = Get-Random -Minimum $minSleep -Maximum $maxSleep
+    Start-Sleep -Seconds $sleepSeconds
+}
+
+# Main loop to simulate user activity (mouse movement)
+while ($true) {
+    Move_Mouse
+}
