b6c0f71f1d36b3989960030453505c2629916c7f
Transcendence -- <77237927+Ruizheee@users.noreply.github.com>
2024-07-15 01:57:52 +0800

    spoof fan and systemenclosure wmi

--- ps_scripts/spoofwmi/spoofwmi.ps1
+++ ps_scripts/spoofwmi/spoofwmi.ps1
@@ -1,3 +1,10 @@
+function ChangeDirectory {
+    # Must be in the same directory as this powershell script
+    # as well as the MOF files to work
+    $scriptInstalledDirectory = $PSScriptRoot
+    Set-Location -Path $scriptInstalledDirectory
+}
+
 function Spoof_Bios { 
     Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "bios.mof") -NoNewWindow -Wait
     $newBios = ([WMIClass]"\\.\root\cimv2:Win32_Bios").CreateInstance()
@@ -39,19 +46,8 @@ function Spoof_VideoController {
     $newVideoController.Put()
 }
 
-function Spoof_Bios { 
-    Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "bios.mof") -NoNewWindow -Wait
-    $newBios = ([WMIClass]"\\.\root\cimv2:Win32_Bios").CreateInstance()
-    $newBios.SMBIOSBIOSVersion = "5FCN95WW"
-    $newBios.Manufacturer = "Phoenix Technologies Ltd."
-    $newBios.Name = "5FCN95WW"
-    $newBios.SerialNumber = "QB06506849OA3OK"
-    $newBios.Version = "LENOVO - 6040000"
-    $newBios.Put()
-}
-
 function Spoof_ComputerSystem {
-    $oldComputerSystem = Get-WmiObject -Class Win32_ComputerSystem
+    $oldComputerSystem = Get-WmiObject -Class Win32_ComputerSystem 
     $totalPhysicalMemory = $oldComputerSystem.TotalPhysicalMemory
     $primaryOwnerName = $oldComputerSystem.PrimaryOwnerName
 
@@ -66,10 +62,72 @@ function Spoof_ComputerSystem {
     $newComputerSystem.Put()
 }
 
+function Spoof_DiskDrive {
+    # Currently only spoofs the first drive
+    # May edit if Sandbox is found to have multiple drives
+    $oldDiskDrive = Get-WmiObject -Class Win32_DiskDrive | where-Object { $_.DeviceID -eq "\\.\PHYSICALDRIVE0" }
+    $partitions = $oldDiskDrive.Partitions
+    $deviceID = $oldDiskDrive.DeviceID
+    $size = $oldDiskDrive.Size
+
+    Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "diskdrive.mof") -NoNewWindow -Wait
+    $newDiskDrive = ([WMIClass]"\\.\root\cimv2:Win32_DiskDrive").CreateInstance()
+    $newDiskDrive.Partitions = $partitions
+    $newDiskDrive.DeviceID = $deviceID
+    $newDiskDrive.Model = "SanDisk SD8SNAT256G1002"
+    $newDiskDrive.Size = $size
+    $newDiskDrive.Caption = "SanDisk SD8SNAT256G1002"
+    $newDiskDrive.Put()
+}
+
+function Spoof_ComputerSystemProduct {
+    Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "computersystemproduct.mof") -NoNewWindow -Wait
+    $newComputerSystemProduct = ([WMIClass]"\\.\root\cimv2:Win32_ComputerSystemProduct").CreateInstance()
+    $newComputerSystemProduct.IdentifyingNumber = "QB06506849OA3OK"
+    $newComputerSystemProduct.Name = "5FCN95WW"
+    $newComputerSystemProduct.Vendor = "Phoenix Technologies Ltd."
+    $newComputerSystemProduct.Version = "1.0"
+    $newComputerSystemProduct.Caption = "Computer System Product"
+    $newComputerSystemProduct.Put()
+}
+
+function Spoof_SystemEnclosure {
+    Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "systemenclosure.mof") -NoNewWindow -Wait
+    $newSystemEnclosure = ([WMIClass]"\\.\root\cimv2:Win32_SystemEnclosure").CreateInstance()
+    $newSystemEnclosure.Manufacturer = "Phoenix Technologies Ltd."
+    $newSystemEnclosure.LockPresent = "True"
+    $newSystemEnclosure.SerialNumber = "CSN12345678901234567"
+    $newSystemEnclosure.SMBIOSAssetTag = "No Asset Tag"
+    $newSystemEnclosure.SecurityStatus = "3"
+    $newSystemEnclosure.Put()
+}
+
+function Spoof_Fan {
+    Start-Process -FilePath "mofcomp.exe" -ArgumentList (Join-Path -Path (Get-Location) -ChildPath "fan.mof") -NoNewWindow -Wait
+    $newFanClass = ([WMIClass]"\\.\root\cimv2:Win32_Fan").CreateInstance()
+    $newFanClass.DeviceID = "root\cimv2 0"
+    $newFanClass.ActiveCooling = "True"
+    $newFanClass.Availability = "3"
+    $newFanClass.Caption = "Cooling Device"
+    $newFanClass.CreationClassName = "Win32_Fan"
+    $newFanClass.Description = "Cooling Device"
+    $newFanClass.Name = "Cooling Device"
+    $newFanClass.Status = "OK"
+    $newFanClass.StatusInfo = "2"
+    $newFanClass.SystemCreationClassName = "Win32_ComputerSystem"
+    $newFanClass.SystemName = [System.Environment]::MachineName
+    $newFanClass.Put()
+}
+
 function main {
+    ChangeDirectory
     Spoof_VideoController
     Spoof_Bios
     Spoof_ComputerSystem
+    Spoof_DiskDrive
+    Spoof_ComputerSystemProduct
+    Spoof_SystemEnclosure
+    Spoof_Fan
 }
 
 main
