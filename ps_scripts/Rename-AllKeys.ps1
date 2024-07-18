6b0fd03b6ce4af07edeb4eb8c56e16944e6927b0
Ninjarku <33929885+Ninjarku@users.noreply.github.com>
2024-07-18 21:39:31 +0800

    Update Rename-AllKeys.ps1

--- ps_scripts/Rename-AllKeys.ps1
+++ ps_scripts/Rename-AllKeys.ps1
@@ -7,20 +7,7 @@ function Rename-RegistryKeys {
     )
 
     foreach ($regPath in $regPaths) {
-        ProcessRegistryKey $childSubKey.PSPath $OldValue $NewValue
-
-        # Get all subkeys non-recursively
-        # $subKeys = Get-ChildItem -Path $regPath -ErrorAction SilentlyContinue
-
-        # foreach ($subKey in $subKeys) {
-            # Process each subkey without recursion
-            # ProcessRegistryKey $subKey.PSPath $OldValue $NewValue
-
-            # # Get and process subkeys one level deep
-            # $childSubKeys = Get-ChildItem -Path $subKey.PSPath -ErrorAction SilentlyContinue
-            # foreach ($childSubKey in $childSubKeys) {
-            # }
-        # }
+        ProcessRegistryKey $regPath $OldValue $NewValue
     }
 }
 
@@ -39,7 +26,6 @@ function ProcessRegistryKey {
         
         foreach ($prop in $propertiesOfInterest) {
             if ($keyProps.$prop -is [string] -and $keyProps.$prop -match [regex]::Escape($OldValue)) {
-            Write-Host "FOUND"
                 # Replace old data with new data
                 $newData = $keyProps.$prop -replace [regex]::Escape($OldValue), $NewValue
                 Set-ItemProperty -Path $keyPath -Name $prop -Value $newData -ErrorAction SilentlyContinue
@@ -67,55 +53,6 @@ function ProcessRegistryKey {
         Write-Host "Failed to access data in $($keyPath): $_"
     }
 }
-
-# function Rename-RegistryKeys {
-#     param (
-#         [string]$OldValue,
-#         [string]$NewValue,
-#         [string]$regPaths
-#     )
-
-#     foreach ($regPath in $regPaths) {
-#         # Get all subkeys non-recursively
-#         $subKeys = Get-ChildItem -Path $regPath -ErrorAction SilentlyContinue
-
-#         foreach ($subKey in $subKeys) {
-#             # Process each subkey without recursion
-#             ProcessRegistryKey $subKey.PSPath $OldValue $NewValue
-
-#             # Get and process subkeys one level deep
-#             $childSubKeys = Get-ChildItem -Path $subKey.PSPath -ErrorAction SilentlyContinue
-#             foreach ($childSubKey in $childSubKeys) {
-#                 ProcessRegistryKey $childSubKey.PSPath $OldValue $NewValue
-#             }
-#         }
-#     }
-# }
-
-# # Function to process a single registry key
-# function ProcessRegistryKey {
-#     param (
-#         [string]$keyPath,
-#         [string]$OldValue,
-#         [string]$NewValue
-#     )
-
-#     try {
-#         # Get the property values
-#         $values = Get-ItemProperty -Path $keyPath -ErrorAction SilentlyContinue
-
-#         foreach ($value in $values.PSObject.Properties) {
-#             if ($value.Value -is [string] -and $value.Value -match [regex]::Escape($OldValue)) {
-#                 # Replace old value with new value
-#                 $newValue = $value.Value -replace [regex]::Escape($OldValue), $NewValue
-#                 Set-ItemProperty -Path $keyPath -Name $value.Name -Value $newValue -ErrorAction SilentlyContinue
-#                 Write-Host "Renamed $OldValue to $NewValue in $($keyPath)"
-#             }
-#         }
-#     } catch {
-#         Write-Host "Failed to access $($keyPath): $_"
-#     }
-# }
 
 
 function RenameQEMUKeys{
