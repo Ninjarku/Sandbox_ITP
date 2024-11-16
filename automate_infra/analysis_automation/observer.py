926636456c2e662e8c9326f2a4da5575899e0c87
Transcendence -- <77237927+Ruizheee@users.noreply.github.com>
2024-11-16 11:59:49 +0800

    updated script and added requirements file

--- automate_infra/analysis_automation/observer.py
+++ automate_infra/analysis_automation/observer.py
@@ -1,23 +1,18 @@
-#Ensure that zipfile, tarfile, requests and watchdog libaries are installed
-#Ensure that password.txt exists if the zip file you intend to copy in consists of a password
-#Ensure that the password can be found in password.txt when extracting Password-Protected zip files
-
 import time
 import os
-import zipfile
+import pyzipper
 import tarfile
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class DirectoryObserver:
-    def __init__(self, directory_path):
+    def __init__(self, observer, directory_path):
         self.directory_path = directory_path
-        self.observer = Observer()
+        self.observer = observer
         
     def start(self):
-        decompressor = Decompressor()
-        self.event_handler = NewFileHandler(decompressor)
+        self.event_handler = NewFileHandler()
         self.observer.schedule(self.event_handler, self.directory_path, recursive=False)
         self.observer.start()
         try:
@@ -30,88 +25,107 @@ class DirectoryObserver:
         self.observer.stop()
         self.observer.join()
         
-class NewFileHandler(FileSystemEventHandler):
-    def __init__(self, decompressor):
-        super().__init__()
-        self.decompressor = decompressor
-
+class NewFileHandler(FileSystemEventHandler):     
     def on_created(self, event):
         if not event.is_directory:
-            while os.path.getsize(event.src_path) == 0:
-            	time.sleep(0.1)
-            	
-            executable_detected = self.decompressor.decompress(event.src_path)
-            scheduler = Scheduler()
+            try:      
+                while os.path.getsize(event.src_path) == 0:
+                    time.sleep(0.1)
 
-            if executable_detected:
-               schedule_response = scheduler.schedule_analysis(executable_detected)
-               print("Task ID: ", schedule_response)
+                decompressor = Decompressor()
+                scheduler = Scheduler()
+                new_decompressed_content = decompressor.decompress(event.src_path)
+
+                if new_decompressed_content: 	
+                    if not isinstance(new_decompressed_content, list):
+                        schedule_response = scheduler.schedule_analysis(new_decompressed_content)
+                        print("Task ID: ", schedule_response)
+                        
+            except Exception as e:
+                print(f"Error: {e}")
             
 class Decompressor:
-    def __init__(self):
+    def __init__(self, password_file='password.txt'):
+        self.script_directory = os.path.dirname(os.path.abspath(__file__))
+        self.password_file = os.path.join(self.script_directory, password_file)
         self.MAGIC_BYTES = {
             b'\x50\x4B\x03\x04': 'ZIP',               # ZIP files
             b'\x1F\x8B': 'GZIP',                      # GZIP files
-            b'\x52\x61\x72\x21\x1A\x07\x00': 'RAR',   # RAR files
             b'\x75\x73\x74\x61\x72': 'TAR',           # TAR files
             b'\x62\x5A': 'BZ2',                       # BZ2 files
-            b'\xFD\x37\x7A\x58\x5A\x00': 'XZ'         # XZ files
         }
-    
-    def identify_file_type(self, filepath):
+
+    def filetype_identification(self, filepath):
+        """
+        Identifies the file type based on its magic bytes
+        """
         with open(filepath, 'rb') as file:
-            magic_bytes_read = file.read(8)
+            magic_bytes_read = file.read(max(len(header) for header in self.MAGIC_BYTES))
             for header, filetype in self.MAGIC_BYTES.items():
                 if magic_bytes_read.startswith(header):
                     return filetype
         return None
 
-    def decompress_zip_files(self, filepath, destination):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        password_file = os.path.join(script_dir, 'password.txt')
-        with zipfile.ZipFile(filepath, 'r') as zip_archive:
-            if any(file_info.flag_bits & 0x1 for file_info in zip_archive.infolist()):
-                with open(password_file, 'r') as pf:
-                    for line in pf:
-                        password = line.strip().encode('utf-8')
-                        try:
-                            zip_archive.extractall(destination, pwd=password)
-                            return zip_archive.namelist()
-                        except (RuntimeError, zipfile.BadZipFile):
-                            continue
-            else:
-                zip_archive.extractall(destination)
-                return zip_archive.namelist()
-
-
-    def decompress_tar_files(self, filepath, destination):
-        with tarfile.open(filepath, 'r:*') as tar_archive:
-            tar_archive.extractall(destination)
-            return tar_archive.getnames()
-
-    def decompress_mapper(self, filetype):
+    def decompress_mapper(self):
+        """
+        Based on the file extension, returns the corresponding method to use for decompression
+        """
         return {
             'ZIP': self.decompress_zip_files,
             'TAR': self.decompress_tar_files,
             'GZIP': self.decompress_tar_files,
             'BZ2': self.decompress_tar_files,
-            'XZ': self.decompress_tar_files
-        }.get(filetype)
+        }
+
+    def decompress_zip_files(self, filepath, destination):
+        """
+        Decompresses or extracts ZIP files
+        """
+        extracted_files = []
+        with pyzipper.AESZipFile(filepath, 'r') as zip_archive:
+            if any(file_info.flag_bits & 0x1 for file_info in zip_archive.infolist()):
+                with open(self.password_file, 'r') as pf:
+                    for line in pf:
+                        password = line.strip().encode('utf-8')
+                        try:
+                            zip_archive.setpassword(password)
+                            zip_archive.extractall(destination)
+                            return zip_archive.namelist()
+                        except RuntimeError:
+                            continue
+            else:
+                zip_archive.extractall(destination)
+                return zip_archive.namelist()
+        
+        return None
+
+    def decompress_tar_files(self, filepath, destination):
+        """
+        Decompresses TAR files
+        """
+        with tarfile.open(filepath, 'r:*') as tar_archive:
+            tar_archive.extractall(destination)
+            return tar_archive.getnames()
 
     def decompress(self, filepath):
-        extension = self.identify_file_type(filepath)
-        destination = os.path.dirname(filepath)
-        relevant_decompress_function = self.decompress_mapper(extension)
+        """
+        Main decompress function
+        """
+        filetype = self.filetype_identification(filepath)
+        if not filetype: # If file is not even archived in the first place
+            return filepath
 
-        if relevant_decompress_function:
-            extracted_files = relevant_decompress_function(filepath, destination)
-            os.remove(filepath)
+        else:
+            decompress_function = self.decompress_mapper().get(filetype)
+            destination = os.path.dirname(filepath)
 
-            for filename in extracted_files:
-                if filename.endswith(".txt"):
-                    executable = os.path.join(destination, filename)
-                    return executable
-        return None 
+            if decompress_function:
+                extracted_files = decompress_function(filepath, destination)
+                os.remove(filepath)
+
+                if extracted_files:
+                    return extracted_files
+                return None
 
 class Scheduler:
     def __init__(self):
@@ -130,6 +144,6 @@ class Scheduler:
 
 if __name__ == "__main__":
     directory_to_monitor = "/home/cape/automation"
-    observer = DirectoryObserver(directory_to_monitor)
+    observer_instance = Observer()
+    observer = DirectoryObserver(observer_instance, directory_to_monitor)
     observer.start()
-	
